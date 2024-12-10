2b21944ab7aeca2b723e4e35061bf6890b7d2906
Aditya <119723334+22MH1A42G1@users.noreply.github.com>
2024-12-10 19:28:01 +0530

    Time: 16 ms (57.57%), Space: 17.2 MB (13.91%) - LeetHub

--- 2981-find-longest-special-substring-that-occurs-thrice-i/2981-find-longest-special-substring-that-occurs-thrice-i.py
+++ 2981-find-longest-special-substring-that-occurs-thrice-i/2981-find-longest-special-substring-that-occurs-thrice-i.py
@@ -1,9 +1,8 @@
 class Solution:
     def maximumLength(self, s: str) -> int:
-        d = defaultdict(int)
-        subs = [''.join(sub) for _, sub in groupby(s)] 
-        for i in subs:                                 
-            d[i]+= 1                                   
-            if len(i) > 1: d[i[1:]]+= 2              
-            if len(i) > 2: d[i[2:]]+= 3              
-        return max(map(len,filter(lambda x: d[x] > 2,d)), default = -1)
+        res = -1
+        for c in {*s}:
+            q = nlargest(3, findall(c+'+', s)) + ['']*2
+            res = max(res, len(q[0])-2, len(q[1])-(q[0]==q[1]), len(q[2]))
+
+        return res or -1
