b39d0d4911c611c8f8a2d0dc089df796111342f9
Aditya <119723334+22MH1A42G1@users.noreply.github.com>
2025-01-01 12:16:15 +0530

    Time: 0 ms (100%), Space: 17.8 MB (9.48%) - LeetHub

--- 1537-maximum-score-after-splitting-a-string/1537-maximum-score-after-splitting-a-string.py
+++ 1537-maximum-score-after-splitting-a-string/1537-maximum-score-after-splitting-a-string.py
@@ -1,6 +1,6 @@
 class Solution:
     def maxScore(self, s: str) -> int:
-        #Brute Force
+        ##Brute Force
         # ans = 0
         # for i in range(0,len(s)-1):
         #     c = 0
@@ -13,7 +13,7 @@ class Solution:
         #     ans = max(ans,c)
         # return ans
 
-        #Count Left 0's & Right 1's
+        ##Count Left 0's & Right 1's
         # o,z,a=s.count("1"),0,0
         # for i in range(len(s)-1):
         #     if s[i] == "1": 
@@ -23,15 +23,21 @@ class Solution:
         #     a = max(a,z+o)
         # return a
 
-        #1 pass
-        o,z,m=0,0,float("-inf")
-        for i in range(len(s)-1):
-            if s[i]=="1":
-                o+=1
-            else:
-                z+=1
-            m = max(m,z-o)
-        if s[-1]=="1":
-            o+=1
-        return m+o
+        ##1 pass
+        # o,z,m=0,0,float("-inf")
+        # for i in range(len(s)-1):
+        #     if s[i]=="1":
+        #         o+=1
+        #     else:
+        #         z+=1
+        #     m = max(m,z-o)
+        # if s[-1]=="1":
+        #     o+=1
+        # return m+o
 
+        o = s.count('1')
+        mx = 0
+        for i in s[:-1]:
+            o += (i=="0")-(i=="1")
+            mx = max(mx,o)
+        return mx
