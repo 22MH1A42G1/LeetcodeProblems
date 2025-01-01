83bd81f882980ff2e6ee8bb04dbe991692b1f119
Aditya <119723334+22MH1A42G1@users.noreply.github.com>
2025-01-01 12:09:24 +0530

    Time: 1 ms (68.37%), Space: 17.8 MB (15.52%) - LeetHub

--- 1537-maximum-score-after-splitting-a-string/1537-maximum-score-after-splitting-a-string.py
+++ 1537-maximum-score-after-splitting-a-string/1537-maximum-score-after-splitting-a-string.py
@@ -1,13 +1,25 @@
 class Solution:
     def maxScore(self, s: str) -> int:
-        ans = 0
-        for i in range(0,len(s)-1):
-            c = 0
-            for j in range(0,i+1):
-                if s[j]=='0': 
-                    c+=1
-            for j in range(i+1,len(s)):
-                if s[j]=='1': 
-                    c+=1
-            ans = max(ans,c)
-        return ans
+        #Brute Force
+        # ans = 0
+        # for i in range(0,len(s)-1):
+        #     c = 0
+        #     for j in range(0,i+1):
+        #         if s[j]=='0': 
+        #             c+=1
+        #     for j in range(i+1,len(s)):
+        #         if s[j]=='1': 
+        #             c+=1
+        #     ans = max(ans,c)
+        # return ans
+
+        #Count Left 0's & Right 1's
+        o,z,a=s.count("1"),0,0
+        for i in range(len(s)-1):
+            if s[i] == "1": 
+                o-=1
+            else: 
+                z+=1
+            a = max(a,z+o)
+        return a
+
