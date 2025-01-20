ddce7b4b426ab86ea5a509cf03bb672a97c39c5e
Aditya <119723334+22MH1A42G1@users.noreply.github.com>
2025-01-20 19:02:17 +0530

    Time: 117 ms (68.77%), Space: 63.4 MB (5.05%) - LeetHub

--- 2685-first-completely-painted-row-or-column/2685-first-completely-painted-row-or-column.py
+++ 2685-first-completely-painted-row-or-column/2685-first-completely-painted-row-or-column.py
@@ -1,6 +1,20 @@
 import numpy as np
 class Solution:
     def firstCompleteIndex(self, arr: List[int], mat: List[List[int]]) -> int:
-        i = np.argsort(arr)
-        m = np.vectorize(lambda x: i[x-1])(mat)
-        return int(min(m.max(axis=0).min(),m.max(axis=1).min()))
+        # i = np.argsort(arr)
+        # m = np.vectorize(lambda x: i[x-1])(mat)
+        # return int(min(m.max(axis=0).min(),m.max(axis=1).min()))
+        m = len(mat)
+        n = len(mat[0])
+        rows = [n]*m
+        cols = [m]*n
+        loc = {}
+        for i in range(m):
+            for j in range(n):
+                loc[mat[i][j]] = (i,j)
+        for i,val in enumerate(arr):
+            row, col = loc[val]
+            rows[row]-=1
+            cols[col]-=1
+            if rows[row]==0 or cols[col]==0:                
+                return i
