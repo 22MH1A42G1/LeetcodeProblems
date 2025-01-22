febe8df4e4bdba5720376a6d5853ccaa66751d3f
Aditya <119723334+22MH1A42G1@users.noreply.github.com>
2025-01-22 13:26:43 +0530

    Time: 682 ms (85.41%), Space: 85 MB (48.23%) - LeetHub

--- 1876-map-of-highest-peak/1876-map-of-highest-peak.py
+++ 1876-map-of-highest-peak/1876-map-of-highest-peak.py
@@ -1,22 +1,22 @@
+from typing import List
+from collections import deque
+
 class Solution:
     def highestPeak(self, isWater: List[List[int]]) -> List[List[int]]:
-        R = len(isWater)
-        C = len(isWater[0])
-        height = [[float('inf')] * C for _ in range(R)]
-        for i in range(R):
-            for j in range(C):
+        rows, cols = len(isWater), len(isWater[0])
+        heights = [[-1] * cols for _ in range(rows)]
+        queue = deque()
+        for i in range(rows):
+            for j in range(cols):
                 if isWater[i][j] == 1:
-                    height[i][j] = 0
-                else:
-                    if i > 0:
-                        height[i][j] = min(height[i][j], height[i - 1][j] + 1)
-                    if j > 0:
-                        height[i][j] = min(height[i][j], height[i][j - 1] + 1)
-        for i in range(R - 1, -1, -1):
-            for j in range(C - 1, -1, -1):
-                if i < R - 1:
-                    height[i][j] = min(height[i][j], height[i + 1][j] + 1)
-                if j < C - 1:
-                    height[i][j] = min(height[i][j], height[i][j + 1] + 1)
-
-        return height
+                    heights[i][j] = 0
+                    queue.append((i, j))
+        directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+        while queue:
+            x, y = queue.popleft()
+            for dx, dy in directions:
+                nx, ny = x + dx, y + dy
+                if 0 <= nx < rows and 0 <= ny < cols and heights[nx][ny] == -1:
+                    heights[nx][ny] = heights[x][y] + 1
+                    queue.append((nx, ny))
+        return heights
