5cd9b34dbdc5ac6274bcb1efe0a199fa7cdb02f2
Aditya <119723334+22MH1A42G1@users.noreply.github.com>
2025-02-06 11:42:52 +0530

    Time: 0 ms (100%), Space: 17.7 MB (67.47%) - LeetHub

--- 0066-plus-one/0066-plus-one.py
+++ 0066-plus-one/0066-plus-one.py
@@ -1,6 +1,15 @@
 class Solution:
-    def plusOne(self, digits: List[int]) -> List[int]:
-        n=int(''.join(map(str,digits)))
-        m=n+1
-        l=[int(i) for i in str(m)]
-        return l
+    def plusOne(self, digits: List[int]) -> List[int]:#[1,2,3]
+        # n = len(digits)#3
+        # for i in range(n - 1, -1, -1):#2,-1,--
+        #     digits[i] += 1
+        #     if digits[i] < 10:
+        #         return digits
+        #     digits[i] = 0
+        # return [1] + digits
+        d = "".join(map(str,digits))
+        e = int(d)
+        f = e + 1
+        h = list(str(f))
+        g = [int(i) for i in h]
+        return g
