fd20e0be24e29a8ee76d8b5431ecc05b6d2d49f8
Aditya <119723334+22MH1A42G1@users.noreply.github.com>
2025-02-08 19:48:57 +0530

    Time: 53 ms (100%), Space: 76.6 MB (75.98%) - LeetHub

--- 2434-design-a-number-container-system/2434-design-a-number-container-system.py
+++ 2434-design-a-number-container-system/2434-design-a-number-container-system.py
@@ -1,28 +1,25 @@
 class NumberContainers:
 
     def __init__(self):
-        # Initializing the defaultdict with SortedSet and the regular dictionary
-        # Map from number to set of indices
-        self.number_to_indices = collections.defaultdict(SortedSet)
-        # Map from index to number
-        self.index_to_number = {}
+        self.indexToNum = {}
+        self.numToIndices = {}
 
     def change(self, index: int, number: int) -> None:
-        # If index already has a number, remove it from the old number's index set
-        if index in self.index_to_number:
-            previous_number = self.index_to_number[index]
-            self.number_to_indices[previous_number].remove(index)
-            if not self.number_to_indices[previous_number]:
-                del self.number_to_indices[previous_number]
-
-        # Update the number and add the index to the new number's set
-        self.index_to_number[index] = number
-        self.number_to_indices[number].add(index)
+        self.indexToNum[index] = number
+        if number not in self.numToIndices:
+            self.numToIndices[number] = []
+        heapq.heappush(self.numToIndices[number], index)
 
     def find(self, number: int) -> int:
-        # Return the smallest index for the given number, or -1 if not found
-        if number in self.number_to_indices and self.number_to_indices[number]:
-            return self.number_to_indices[number][0]
+        if number not in self.numToIndices:
+            return -1
+        pq = self.numToIndices[number]        
+        while pq:
+            currIndex = pq[0]
+            if self.indexToNum[currIndex] != number:
+                heapq.heappop(pq)
+            else:
+                return currIndex
         return -1
 
 
@@ -30,3 +27,8 @@ class NumberContainers:
 # obj = NumberContainers()
 # obj.change(index,number)
 # param_2 = obj.find(number)
+
+"""
+- number -> heap of indices
+- index -> value at that index (to validate)
+"""
