4311f3f771cf9cd4af70c665e59566353831fe69
Aditya <119723334+22MH1A42G1@users.noreply.github.com>
2025-02-09 18:46:02 +0530

    Time: 0 ms (100%), Space: 29.2 MB (8.29%) - LeetHub

--- 2481-strictly-palindromic-number/2481-strictly-palindromic-number.py
+++ 2481-strictly-palindromic-number/2481-strictly-palindromic-number.py
@@ -1,4 +1,14 @@
 import numpy as np
 class Solution:
     def isStrictlyPalindromic(self, n: int) -> bool:
-        return False
+        def baseSwitch(i, n):
+            res = ""
+            while n != 0:
+                res += str(n % i)
+                n = n // i
+            return res[::-1]
+        for i in range(2, n-1):
+            strBase = baseSwitch(i, n)
+            if strBase != strBase[::-1]:
+                return False
+        return True
