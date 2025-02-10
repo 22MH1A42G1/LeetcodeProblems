cc2cb32e459a690c5616ab0518e201b8f3cb4bd9
Aditya <119723334+22MH1A42G1@users.noreply.github.com>
2025-02-10 20:22:11 +0530

    Time: 0 ms (100%), Space: 17.8 MB (43.55%) - LeetHub

--- 3447-clear-digits/3447-clear-digits.py
+++ 3447-clear-digits/3447-clear-digits.py
@@ -2,8 +2,8 @@ class Solution:
     def clearDigits(self, s: str) -> str:
         l=[]
         for i in s:
-            if i.isdigit() and l:
-                l.pop()
-            else:
+            if(i.isalpha()):
                 l.append(i)
+            else:
+                l.pop()
         return "".join(l)
