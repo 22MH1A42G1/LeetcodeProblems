ee03678d66114e74de31a73992cdb9fc3e2cf0eb
Aditya <119723334+22MH1A42G1@users.noreply.github.com>
2025-03-04 12:49:37 +0530

    Time: 0 ms (100%), Space: 17.6 MB (88.25%) - LeetHub

--- 1889-check-if-number-is-a-sum-of-powers-of-three/1889-check-if-number-is-a-sum-of-powers-of-three.py
+++ 1889-check-if-number-is-a-sum-of-powers-of-three/1889-check-if-number-is-a-sum-of-powers-of-three.py
@@ -1,15 +1,15 @@
 class Solution:
     def checkPowersOfThree(self, n: int) -> bool:
-        if n==12 or n==91 or n==10 or n==27 or n==28 or n==36 or n==37:
-            return True
-        if n==22 or n==23 or n==25 or n==26 or n==44 or n==49 or n==50: 
-            return False
-        if n==68 or n==76 or n==77 or n==95224 or n==2 or n==3985807:
-            return False
-        if n==81 or n==85 or n==93 or n==7627 or n==6378022 or n==6574365:
-            return True
+        # if n==12 or n==91 or n==10 or n==27 or n==28 or n==36 or n==37:
+        #     return True
+        # if n==22 or n==23 or n==25 or n==26 or n==44 or n==49 or n==50: 
+        #     return False
+        # if n==68 or n==76 or n==77 or n==95224 or n==2 or n==3985807:
+        #     return False
+        # if n==81 or n==85 or n==93 or n==7627 or n==6378022 or n==6574365:
+        #     return True
         while n>0:
-            if n%3==0:
+            if n%3==2:
                 return False
             n//=3
         return True
