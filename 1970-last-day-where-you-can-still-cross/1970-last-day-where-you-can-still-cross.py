86721aa5e2822947fdb42c11170e5403192e8fe8
Aditya <119723334+22MH1A42G1@users.noreply.github.com>
2025-12-31 12:25:10 +0530

    Time: 1475 ms (24.31%), Space: 27.3 MB (36.46%) - LeetHub

--- 1970-last-day-where-you-can-still-cross/1970-last-day-where-you-can-still-cross.py
+++ 1970-last-day-where-you-can-still-cross/1970-last-day-where-you-can-still-cross.py
@@ -1,12 +1,34 @@
 class Solution:
-    def latestDayToCross(self, r: int, c: int, a: List[List[int]]) -> int:
-        def check(k):
-            def dfs(i,j,g={*map(tuple,a[:k+1])}):
-                if not (0<i<=r and 0<j<=c) or (i,j) in g: return 1 # stuck
-                if i==r: return 0 # pass
-                g.add((i,j))
-                return all(starmap(dfs,((i,j+1),(i+1,j),(i-1,j),(i,j-1))))
-            
-            return all(dfs(1,j+1) for j in range(c))
-
-        return bisect_left(range(len(a)),1,key=check)
+    def canCross(self,row,col,cells,day):
+            grid=[[0]*col for _ in range(row)]
+            for i in range(day):
+                r, c = cells[i]
+                grid[r-1][c-1]=1
+            queue = deque()
+            visited = set()
+            for c in range(col):
+                if grid[0][c]==0:
+                    queue.append((0,c))
+                    visited.add((0,c))
+            directions = [(0,1),(0,-1),(1,0),(-1,0)]
+            while queue:
+                r, c = queue.popleft()
+                if r == row - 1:
+                    return True
+                for dr, dc in directions:
+                    nr,nc = r+dr,c+dc
+                    if 0<=nr<row and 0<=nc<col and grid[nr][nc]==0 and (nr,nc) not in visited:
+                        visited.add((nr,nc))
+                        queue.append((nr,nc))
+            return False
+    def latestDayToCross(self, row: int, col: int, cells: List[List[int]]) -> int:
+        left, right = 1, len(cells)
+        ans = 0
+        while left <= right:
+            mid = (left + right) // 2
+            if self.canCross(row,col, cells, mid):
+                ans = mid
+                left = mid + 1
+            else:
+                right = mid - 1
+        return ans 
