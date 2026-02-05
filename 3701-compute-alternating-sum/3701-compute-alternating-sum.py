90f9e0494fd17b50a15548d75214b328deb0d228
Aditya <119723334+22MH1A42G1@users.noreply.github.com>
2026-02-05 14:44:14 +0530

    Time: 0 ms (100%), Space: 19.3 MB (29.69%) - LeetHub

--- 3701-compute-alternating-sum/3701-compute-alternating-sum.py
+++ 3701-compute-alternating-sum/3701-compute-alternating-sum.py
@@ -1,11 +1,3 @@
 class Solution:
     def alternatingSum(self, nums: List[int]) -> int:
-        c=0
-        e=[]
-        o=[]
-        for i in range(len(nums)):
-            if i%2==0:
-                e.append(nums[i])
-            else:
-                o.append(nums[i])
-        return sum(e)-sum(o)
+        return sum(nums[::2])-sum(nums[1::2])
