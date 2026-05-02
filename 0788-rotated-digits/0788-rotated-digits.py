2a7b9c5ca2912c2abf0dd9bdf38d2181eb715c06
Aditya <119723334+22MH1A42G1@users.noreply.github.com>
2026-05-02 15:40:26 +0530

    Time: 11 ms (86.76%), Space: 19.1 MB (94.1%) - LeetHub

--- 0788-rotated-digits/0788-rotated-digits.py
+++ 0788-rotated-digits/0788-rotated-digits.py
@@ -1,6 +1,10 @@
 class Solution:
     def rotatedDigits(self, n: int) -> int:
-        return sum(
-            any(c in '2569' for c in s) and not any(c in '347' for c in s)
-            for s in map(str, range(1, n+1))
-        )
+        res = 0
+        for num in range(1, n + 1):
+            curr = str(num)
+            if "3" in curr or "4" in curr or "7" in curr:
+                continue
+            if "2" in curr or "5" in curr or "9" in curr or "6" in curr:
+                res += 1
+        return res
