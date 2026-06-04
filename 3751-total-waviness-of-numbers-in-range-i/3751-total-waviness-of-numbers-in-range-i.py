267add51904d9cc0d1674ecf91661840e476e80f
Aditya <119723334+22MH1A42G1@users.noreply.github.com>
2026-06-04 09:25:00 +0530

    Time: 251 ms (80.08%), Space: 19.3 MB (69.73%) - LeetHub

--- 3751-total-waviness-of-numbers-in-range-i/3751-total-waviness-of-numbers-in-range-i.py
+++ 3751-total-waviness-of-numbers-in-range-i/3751-total-waviness-of-numbers-in-range-i.py
@@ -1,18 +1,17 @@
 class Solution:
     def totalWaviness(self, num1: int, num2: int) -> int:
-        p=(num1,num2)
-        def wavi(x):
-            s=str(x)
+        def w(n):
+            s = str(n)
             if len(s)<3: return 0
-            w=0
+            w = 0
             for i in range(1,len(s)-1):
-                l=int(s[i-1])
-                m=int(s[i])
-                r=int(s[i+1])
+                l=s[i-1]
+                m=s[i]
+                r=s[i+1]
                 if m>l and m>r: w+=1
                 elif m<l and m<r: w+=1
             return w
-        t=0
-        for n in range(num1,num2+1):
-            t+=wavi(n)
-        return t
+        res = 0
+        for i in range(num1,num2+1):
+            res+=w(i)
+        return res
