5df5f7e58feac302848352272fea7df719373bbf
Aditya <119723334+22MH1A42G1@users.noreply.github.com>
2026-06-19 23:32:30 +0530

    Time: 0 ms (100%), Space: 19.4 MB (15.4%) - LeetHub

--- 1732-find-the-highest-altitude/1732-find-the-highest-altitude.py
+++ 1732-find-the-highest-altitude/1732-find-the-highest-altitude.py
@@ -3,5 +3,5 @@ class Solution:
         prefix_sum = [0]*(len(gain)+1)
         for i in range(1,len(gain)+1):
             prefix_sum[i] = prefix_sum[i-1] + gain[i-1]
-        print(prefix_sum)
+        # print(prefix_sum)
         return max(prefix_sum)
