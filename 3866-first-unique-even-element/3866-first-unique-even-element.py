b25d703478c5b4d65ab1e7e9a98509beff63c1a3
Aditya <119723334+22MH1A42G1@users.noreply.github.com>
2026-07-18 12:40:21 +0530

    Time: 0 ms (100%), Space: 19.3 MB (62.44%) - LeetHub

--- 3866-first-unique-even-element/3866-first-unique-even-element.py
+++ 3866-first-unique-even-element/3866-first-unique-even-element.py
@@ -1,8 +1,9 @@
 class Solution:
     def firstUniqueEven(self, nums: list[int]) -> int:
+        even = Counter(nums)
         ans = -1
-        for i in nums:
-            if i%2==0 and nums.count(i)==1:
+        for i,j in even.items():
+            if i%2==0 and j==1:
                 ans = i
                 break
         return ans
