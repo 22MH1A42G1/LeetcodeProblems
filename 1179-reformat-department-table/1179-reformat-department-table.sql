375e6618d2791237761da8daeb6bc3ce1657377c
Aditya <119723334+22MH1A42G1@users.noreply.github.com>
2026-07-22 17:28:10 +0530

    Time: 578 ms (29.79%), Space: 0B (100%) - LeetHub

--- 1179-reformat-department-table/1179-reformat-department-table.sql
+++ 1179-reformat-department-table/1179-reformat-department-table.sql
@@ -1,19 +1,16 @@
-# Write your MySQL query statement below
 SELECT
-  id,
-  SUM(IF (month = "Jan", revenue, null)) AS Jan_Revenue,
-  SUM(IF (month = "Feb", revenue, null)) AS Feb_Revenue,
-  SUM(IF (month = "Mar", revenue, null)) AS Mar_Revenue,
-  SUM(IF (month = "Apr", revenue, null)) AS Apr_Revenue,
-  SUM(IF (month = "May", revenue, null)) AS May_Revenue,
-  SUM(IF (month = "Jun", revenue, null)) AS Jun_Revenue,
-  SUM(IF (month = "Jul", revenue, null)) AS Jul_Revenue,
-  SUM(IF (month = "Aug", revenue, null)) AS Aug_Revenue,
-  SUM(IF (month = "Sep", revenue, null)) AS Sep_Revenue,
-  SUM(IF (month = "Oct", revenue, null)) AS Oct_Revenue,
-  SUM(IF (month = "Nov", revenue, null)) AS Nov_Revenue,
-  SUM(IF (month = "Dec", revenue, null)) AS Dec_Revenue
-FROM
-  Department
-GROUP BY
-  id;
+    id,
+    SUM(CASE WHEN month = 'Jan' THEN revenue END) AS Jan_Revenue,
+    SUM(CASE WHEN month = 'Feb' THEN revenue END) AS Feb_Revenue,
+    SUM(CASE WHEN month = 'Mar' THEN revenue END) AS Mar_Revenue,
+    SUM(CASE WHEN month = 'Apr' THEN revenue END) AS Apr_Revenue,
+    SUM(CASE WHEN month = 'May' THEN revenue END) AS May_Revenue,
+    SUM(CASE WHEN month = 'Jun' THEN revenue END) AS Jun_Revenue,
+    SUM(CASE WHEN month = 'Jul' THEN revenue END) AS Jul_Revenue,
+    SUM(CASE WHEN month = 'Aug' THEN revenue END) AS Aug_Revenue,
+    SUM(CASE WHEN month = 'Sep' THEN revenue END) AS Sep_Revenue,
+    SUM(CASE WHEN month = 'Oct' THEN revenue END) AS Oct_Revenue,
+    SUM(CASE WHEN month = 'Nov' THEN revenue END) AS Nov_Revenue,
+    SUM(CASE WHEN month = 'Dec' THEN revenue END) AS Dec_Revenue
+FROM Department
+GROUP BY id;
