e543482de2a88b03c73f2c7ad4c5f75fb718ff57
Aditya <119723334+22MH1A42G1@users.noreply.github.com>
2026-07-26 19:41:00 +0530

    Time: 0 ms (100%), Space: 19 MB (98.63%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -3,14 +3,4 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        a,b,c=0,0,0
-        for i in nums:
-            if i==0:
-                a+=1
-            if i==1:
-                b+=1
-            if i==2:
-                c+=1
-        nums[:a] = [0] * a
-        nums[a:a+b] = [1] * b
-        nums[a+b:] = [2] * c
+        nums[:]=sorted(nums)
